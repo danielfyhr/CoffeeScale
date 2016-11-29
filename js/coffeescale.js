330f455f725c812d581a7f25cd559a3f2423dba2
Daniel Fyhr <danielfyhr@gmail.com>
2016-11-29 21:46:21 +0100

    make timer work better

--- js/coffeescale.js
+++ js/coffeescale.js
@@ -9,11 +9,18 @@
 		}
 	}]);
 
-	app.controller('CoffeeController', ['$timeout', function($timeout) {
+	app.filter('secondsToDateTime', [function() {
+    	return function(seconds) {
+        	return new Date(1970, 0, 1).setSeconds(seconds);
+    	};
+	}]);
+
+	app.controller('CoffeeController', ['$scope', '$timeout', function($scope, $timeout) {
 		
 		this.coffeeTypes = coffeeTypes;
 		this.coffeeWeight = coffeeWeight;
 		this.selectedCoffeeType = selectedCoffeeType;
+		$scope.countDown = selectedCoffeeType.time; //initiate countDown
 		
 		this.selectCoffeeType = function(setCoffeeType) {
 			this.selectedCoffeeType = setCoffeeType;
@@ -53,19 +60,53 @@
 
 		};
 
-		this.startTimer = function () {
+		var timeoutFunction = null; //replacable with destroyTimer(); ?
+
+		$scope.startTimer = function() {
 			console.log("startTimer called");
-		}
-		this.pauseTimer = function () {
-			console.log("pauseTimer  called");
-		}
-		this.resetTimer = function () {
-			console.log("resetTimer called");
+			//console.log("$scope.countDown: " + $scope.countDown);
+			//initiate the timer if it doesn't already exist
+			if (timeoutFunction == null) {
+				console.log("timeOutfunction doesn't already exist so runTimer()!");
+				$scope.runTimer();
+			} else {
+				console.log("startTimer does nothing at this point cause timer is already running.");
+			}
 		}
 
-		this.getTime = function() {
-			return selectedCoffeeType.time;
-		};
+		$scope.runTimer = function() {
+			$scope.countDown--;
+			console.log("runTimer: " + timeoutFunction);
+			timeoutFunction = $timeout($scope.runTimer,1000);
+
+			if ($scope.countDown == 0) {
+				window.alert('Kaffet är färdigt bror!')
+				$timeout.cancel(timeoutFunction);
+			}
+		}
+
+		$scope.pauseTimer = function() {
+			console.log("pauseTimer called");
+			if (timeoutFunction != null) {
+				$timeout.cancel(timeoutFunction);
+				console.log("and there was something to cancel so it's cancelled now.");
+				$scope.destroyTimer();
+			}
+		}
+
+		$scope.resetTimer = function() {
+			console.log("resetTimer called");
+			//reset countDown by setting it to selected coffeetype's time.
+			console.log("trying to reset countDown from " + $scope.countDown + " to " + selectedCoffeeType.time);
+			//re-initiate countDown
+			$scope.countDown = selectedCoffeeType.time; 
+			//pauseTimer
+			$scope.pauseTimer();
+		}
+
+		$scope.destroyTimer = function() {
+			timeoutFunction = null;
+		}
 
 	}]);
 
@@ -75,7 +116,7 @@
 				description: '<p>Makes a concentrated coffee which you can serve warm by adding boiling water.</p><p>Put the grind in a bowl and soak everything with the given amount of cold water. Cover with cling film and let it seep in room temperature for ~12 hours. Filter out the grind and store it in the fridge.</p><p>To serve preheat a cup, then measure <strong>1/6</strong> of the coldbrew and top with <strong>5/6</strong> boiling water.</p>',
 				waterMultiplier: 6,
 				servingsMultiplier: 10,
-				time: 240
+				time: 120
 			},
 			{
 				name: 'Frenchpress',
